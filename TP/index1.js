899f134b44de68b4f10d3edcc9424c1d7631234b
Rayan-OK <70883286+Rayan-OK@users.noreply.github.com>
2021-02-12 10:39:25 +0100

    TP1 OK //

--- TP/index1.js
+++ TP/index1.js
@@ -4,26 +4,22 @@ const url = require('url');
 const qs = require('querystring');
 
 
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World ! :)</p>' +
-'    </body>' +
-'</html>';
-
 const serverHandle = function (req, res) {
-  // Retrieve and print the queryParams
-  const queryParams = qs.parse(url.parse(req.url).query);
-  console.log(queryParams);
+  const route = url.parse(req.url);
+  const path = route.pathname;
+  const params = qs.parse(route.query);
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name']);
+  } else {
+    res.write('Hello anonymous');
+  }
+  
   res.end();
 }
+
 
 const server = http.createServer(serverHandle);
 server.listen(8080);
