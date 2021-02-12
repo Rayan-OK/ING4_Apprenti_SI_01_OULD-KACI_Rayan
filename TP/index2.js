fdbd09c46d9e9465e495bf5886ac091e2504e643
Rayan-OK <70883286+Rayan-OK@users.noreply.github.com>
2021-02-12 11:23:06 +0100

    Evo. index2

--- TP/index2.js
+++ TP/index2.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const response = {
+    message: 'Hello World ! :D',
+  };
+
+  return res.status(200).json(response);
+  //Vous pouvez aussi faire à la place de la ligne précedente : return res.status(200).json(response);
 });
 
 app.listen(8080, function() {
