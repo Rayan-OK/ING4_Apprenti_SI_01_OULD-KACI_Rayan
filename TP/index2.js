8a36709e230b2f8013d8d502e7fbd1915407bd5a
Rayan-OK <70883286+Rayan-OK@users.noreply.github.com>
2021-02-17 23:15:59 +0100

    TP 2 OK //

--- TP/index2.js
+++ TP/index2.js
@@ -1,37 +1,71 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  const response = {
-    message: 'Hello World ! :D',
-  };
+const path=require("path");
 
-  return res.status(200).json(response);
-  //Vous pouvez aussi faire à la place de la ligne précedente : return res.status(200).json(response);
+const bodyParser=require("body-parser");
+
+
+const router = express.Router();
+
+
+app.use(express.static('file2'));
+app.use(bodyParser());
+
+
+router.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, "file2/app.html"));
 });
 
-app.get('/users', function(req, res) {
-    const users = [
-      {
-         id: '1234',
-         name: 'user1',
-      },
-      {
-        id: '1234',
-        name: 'user1',
-      },
-    ];
-    
-    return res.status(200).json(users);
-  });
+router.get('/rayan', (req, res) =>{
+  res.json("You are viewing my profile !");
+});
 
-  app.get('/users/:id', function(req, res) {
-    const userId = req.params.id;
-    const userName = req.params.id;
-    
-    return res.statut(200).json(userName);
-  });
-  
-  app.listen(8080, function() {
-    console.log(`server listening on 8080`);
-  });
+router.get('/delete', (req, res)=>{
+  res.sendFile(path.join(__dirname, "file2/app.html"));
+
+  console.log(list);
+});
+
+let user1 = {
+  username:"Mercury",
+  address:"Solar System",
+};
+
+let list=[user1];
+router.post('/user',(req,res)=>{
+
+  console.log("Req ", req.body);
+  const{username,address}=req.body;
+
+  const user={
+    username:username,
+    address:address,
+  }
+
+  list.push(user);
+  res.json({msg:"We receive your data", data:list});
+});
+
+router.post('delete/user', (req, res)=>{
+  console.log("Req", req.body);
+  const position = req.body;
+  list.splice(position,1);
+  console.log("Liste:",list);
+  res.json({msg:"User deleted successfully", data:list});
+})
+
+
+
+
+
+router.get("*", (req, res)=>{
+  res.json("Page not found...");
+});
+
+app.use("/", router);
+
+app.listen(8080, () =>{
+  console.log("App is startint at port", 8080);
+}); //required
