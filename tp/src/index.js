8c027ac0cfdf586649b16a3f9c020b6250177c28
Rayan-OK <70883286+Rayan-OK@users.noreply.github.com>
2021-02-26 10:43:36 +0100

    Update index.js

--- tp/src/index.js
+++ tp/src/index.js
@@ -38,8 +38,14 @@ constructor(props){
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -52,7 +58,14 @@ constructor(props){
     }
   
     render() {
-      const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner){
+            status = winner + 'a gagné';
+        } else {
+        const status = 'Next player: ' +
+        (this.state.xIsNext ? 'X' : 'O');
+        }
   
       return (
         <div>
@@ -100,3 +113,22 @@ constructor(props){
     document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
